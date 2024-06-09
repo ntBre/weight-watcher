4197bb478d2123ff4d06b23750db9e00791e91a1
Brent Westbrook <brentrwestbrook@gmail.com>
2024-06-09 15:45:06 -0400

    add templates, redirect, handle weight endpoint

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ impl Display for ContentType {
 
 struct Response {
     status: usize,
+    location: Option<&'static str>,
     content_type: ContentType,
     body: String,
 }
@@ -28,6 +29,25 @@ impl Response {
             status: 200,
             body: String::new(),
             content_type: ContentType::Html,
+            location: None,
+        }
+    }
+
+    fn redirect(to: &'static str) -> Self {
+        Self {
+            status: 303,
+            location: Some(to),
+            body: String::new(),
+            content_type: ContentType::Html,
+        }
+    }
+
+    fn err() -> Self {
+        Self {
+            status: 404,
+            body: String::new(),
+            content_type: ContentType::Html,
+            location: None,
         }
     }
 
@@ -39,6 +59,8 @@ impl Response {
     fn reason(&self) -> &'static str {
         match self.status {
             200 => "OK",
+            303 => "See Other",
+            404 => "Not Found",
             _ => "",
         }
     }
@@ -51,30 +73,54 @@ impl Response {
 impl Display for Response {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "HTTP/1.1 {} {}\r\n", self.status, self.reason())?;
-        write!(f, "Content-Type: {}\r\n", self.content_type)?;
+        if let Some(location) = self.location {
+            write!(f, "Location: {}", location)?;
+        } else {
+            write!(f, "Content-Type: {}\r\n", self.content_type)?;
+        }
         write!(f, "\r\n")?;
         write!(f, "{}", self.body)?;
         Ok(())
     }
 }
 
-fn handle_client(mut stream: TcpStream) {
+fn dispatch(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request: Vec<_> = buf_reader
         .lines()
         .map(Result::unwrap)
         .take_while(|line| !line.is_empty())
         .collect();
-    println!("{}", request[0]);
-    let response = Response::ok().body("<h1>Hello, world!</h1>".into());
+    assert!(request.len() >= 1);
+    let fields: Vec<_> = request[0].split_ascii_whitespace().collect();
+    assert!(fields.len() == 3);
+    let url = fields[1];
+    let parts: Vec<_> = url.split('?').collect();
+    assert!(matches!(parts.len(), 1 | 2));
+    let response = match parts[0] {
+        "/" => index(),
+        "/weight" if parts.len() == 2 => weight(parts[1]),
+        _ => {
+            Response::err().body(include_str!("../templates/error.html").into())
+        }
+    };
     stream.write_all(&response.as_bytes()).unwrap();
+}
+
+fn index() -> Response {
+    Response::ok().body(include_str!("../templates/index.html").into())
+}
+
+fn weight(query: &str) -> Response {
+    dbg!(query);
+    Response::redirect("/")
 }
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:9999")?;
 
     for stream in listener.incoming().map(Result::unwrap) {
-        handle_client(stream);
+        dispatch(stream);
     }
     Ok(())
 }
